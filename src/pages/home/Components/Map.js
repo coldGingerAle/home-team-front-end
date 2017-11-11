384ebb64f7d95521b27ef4906e3c7f9d534853c5
KevinCybura <KevinCybura@gmail.com>
2017-11-10 20:23:03 -0500

    added markers

--- src/pages/home/Components/Map.js
+++ src/pages/home/Components/Map.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-const Listing = (props) => {
- return ( <div>
-    
-  </div>)
-}
 export class MapContainer extends React.Component {
-  fetchPlaces = (mapProps, map) => {
-    const {google} = mapProps;
-    const service = new google.maps.places.PlacesService(map);
-    console.log(mapProps);
+  constructor(props){
+    super(props);
+    this.state = {
+      places : [{lat: 37.778519, lng: -122.405640},{lat: 37.759703, lng: -122.428093},{lat: 37.762391, lng: -122.439192},{}]
+    }
   }
   render() {
     const style = {
-      width: '100%',
-      height: '100%'
-    }
-    
+      width: '75%',
+      height: '75%'
+    };
+
     return (
       <Map
         google={this.props.google}
         style={style}
         initialCenter={{
           lat: 40.730610,
-          lng: -73.935242,
+          lng: -73.935242
         }}
         zoom={17}
         onClick={this.onMapClicked}
-      onReady={this.fetchPlaces}
-      visible={true}>
-        <Listing  />
-    
+        
+        visible={true}
+      >
+        {this.state.places.map((place,index) => {
+          return <Marker key={index}
+          position={{lat:place.lat, lng: place.lng}} />
+        })}
         <Marker onClick={this.onMarkerClick} name={'Current location'} />
 
         <InfoWindow onClose={this.onInfoWindowClose}>

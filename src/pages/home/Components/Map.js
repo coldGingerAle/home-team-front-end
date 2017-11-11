ef34eddc6cf94b977f9e8cec54f792412174cf7c
coldGingerAle <hui.lin69@qmail.cuny.edu>
2017-11-10 23:33:19 -0500

    Connected to OpenData API for wifi hotspots and plotted locations on Google Maps.

--- src/pages/home/Components/Map.js
+++ src/pages/home/Components/Map.js
@@ -5,9 +5,30 @@ export class MapContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      places : [{lat: 37.778519, lng: -122.405640},{lat: 37.759703, lng: -122.428093},{lat: 37.762391, lng: -122.439192},{}]
+      places : [{lat: 37.778519, lng: -122.405640},{lat: 37.759703, lng: -122.428093},{lat: 37.762391, lng: -122.439192}],
+      wifihotspots: []
     }
   }
+
+  componentDidMount() {
+    var self = this;
+    fetch('https://data.cityofnewyork.us/resource/24t3-xqyv.json')
+      .then(res => res.json())
+      .then(hotspots => {
+        let hotspotsArray = hotspots.map(hotspot => {
+          let hotspot_lat_long = {
+            lat: hotspot.location_lat_long.coordinates[1],
+            lng: hotspot.location_lat_long.coordinates[0]
+          }
+          return hotspot_lat_long;
+        })
+        self.setState({
+          wifihotspots: hotspotsArray
+        })
+        console.log(self.state.wifihotspots)
+      })
+  }
+
   render() {
     const style = {
       width: '75%',
@@ -19,20 +40,18 @@ export class MapContainer extends React.Component {
         google={this.props.google}
         style={style}
         initialCenter={{
-          lat: 40.730610,
-          lng: -73.935242
+          lat: 40.745968,
+          lng: -73.994039
         }}
         zoom={17}
         onClick={this.onMapClicked}
-        
+
         visible={true}
       >
-        {this.state.places.map((place,index) => {
+        {this.state.wifihotspots.map((place,index) => {
           return <Marker key={index}
           position={{lat:place.lat, lng: place.lng}} />
         })}
-        <Marker onClick={this.onMarkerClick} name={'Current location'} />
-
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
             <h1 />

03bd34c6ebf2ab838183617cd879f4af05359e08
Yvan Pangilinan <yvanp@Yvans-MacBook-Pro.local>
2017-11-12 13:06:13 -0500

    fixed footer icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ class Footer extends Component {
               <img className="img-responsive img-wrap-footer" src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github"></img>
               <img className="img-responsive img-wrap-footer " src="https://marketing-challengepost.netdna-ssl.com/assets/reimagine2/devpost-icon-c8f52fc37bef931ff121230872382167.svg" alt="devpost"></img>
               <img className="img-responsive img-wrap-footer" src="http://www.adweek.com/agencyspy/wp-content/uploads/sites/7/2015/07/chase-logo.png" alt="chase"></img>
-              <img className="img-responsive img-wrap-footer" src="http://1000logos.net/wp-content/uploads/2017/02/IBM-Logo.png" alt="ibm"></img>
+              <img className="img-responsive img-wrap-footer" src="http://www.dbtekpro.com/wp-content/uploads/2015/07/bluemix-logo.png" alt="ibm"></img>
             </ul>
             </div>
           </div>
